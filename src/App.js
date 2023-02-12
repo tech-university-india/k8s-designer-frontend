cf677b57dabc524c55c74623c57a7e169413dc41
ReeKid2002 <rahulbehera2002@gmail.com>
2023-02-12 09:27:27 +0530

    feat: add route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
-
+import { Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import { Button } from '@mui/material';
 function App() {
   return (
-    <div className='App'>
-      <h1 className='heading'>HELLO 👋, WORLD 🌏!</h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<div className='App'>
+        <h1 className='heading'>HELLO 👋, WORLD 🌏!</h1>
+        <div className='buttons'>
+          <Link to='/home'><Button variant='contained'>Home</Button></Link>
+          <Link to='/about'><Button variant='contained'>About</Button></Link>
+        </div>
+      </div>}></Route>
+      <Route path='/home' element={<Home />}></Route> 
+      <Route path='/about' element={<About />}></Route>
+    </Routes>
   );
 }
 
