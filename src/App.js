4b6e985e025d1d2d0e15a894467ca3660b105cef
shashwath sk <shashukarkera01@gmail.com>
2023-03-02 22:57:31 +0530

    feat: add routing for configuration page

--- src/App.js
+++ src/App.js
@@ -27,6 +27,8 @@ function App() {
       <Route path = '/configuration' element = {<ConfigurationPage/>}></Route>
       {/* <Route path='/' element={<Footer />}></Route>  */}
       <Route path='/dashboard' element={<ReactFlowProvider><MainDashboard /></ReactFlowProvider>}></Route>
+      <Route path = '/configuration' element = {<ConfigurationPage/>}></Route>
+      {/* <Route path='/' element={<Footer />}></Route>  */}
     </Routes>
   );
 }
