0320d4e6e8561b957da30cf78a03941fc91e2d75
shashwath sk <shashukarkera01@gmail.com>
2023-03-02 23:01:52 +0530

    feat: add component that renders header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export default function Header(){
+  return(
+    <div className="header">
+      <div className="title">
+        <h1> K8s Designer</h1>
+      </div>
+      {/* <div className="user-info">
+        
+      </div> */}
+    </div>
+  );
+}
