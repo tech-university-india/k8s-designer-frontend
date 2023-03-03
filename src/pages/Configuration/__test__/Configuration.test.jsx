21f524b940f5602ebebb182f8d4eb0f77fc36e0a
shashwath sk <shashukarkera01@gmail.com>
2023-03-03 16:21:04 +0530

    chore: clear commits  used for testing useState scenarios

--- src/pages/Configuration/__test__/Configuration.test.jsx
+++ src/pages/Configuration/__test__/Configuration.test.jsx
@@ -1,5 +1,7 @@
 import {render ,screen } from '@testing-library/react';
 import Configuration from '../index';
+import * as React from 'react';
+
 
 describe('Configuration',()=>{
 
@@ -11,14 +13,5 @@ describe('Configuration',()=>{
   it('should render Frontend configuration component when rendered with service value equal to frontend',()=>{
     render(<Configuration/>);
     expect(screen.queryByText('FRONTEND MICROSERVICES')).toBeTruthy();
-  });
-
-  // it('should fetch data from local storage and set to state when rendered for 1st time',()=>{
-  //   const stateFunction = jest.fn((feConfigCache)=>{
-  //     console.log(feConfigCache);
-  //   });
-  //   render(<Configuration/>);
-  //   expect(stateFunction).toHaveBeenCalled();
-  // });
-  
+  });  
 });
