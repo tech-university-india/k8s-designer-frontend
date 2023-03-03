ad60913a3ac88c15abb00eeb5f6482f6bcf342f3
shashwath sk <shashukarkera01@gmail.com>
2023-03-03 16:22:46 +0530

    chore: add test to check textfield value are updated on change

--- src/components/FrontendConfig/__test__/FrontendConfig.test.jsx
+++ src/components/FrontendConfig/__test__/FrontendConfig.test.jsx
@@ -2,9 +2,10 @@ import {fireEvent, render ,screen,waitFor } from '@testing-library/react';
 import FrontendConfig from '..';
 import * as React from 'react';
 import FeConfigHandler from '../../../utils/FeConfigHandler';
-// import RepoConfigHandler from '../../../../utils/RepoConfigHandler';
+import RepoConfigHandler from '../../../utils/RepoConfigHandler/index';
 
-// jest.mock('../../../../utils/RepoConfigHandler');
+
+jest.mock('../../../utils/RepoConfigHandler/index');
 jest.mock('../../../utils/FeConfigHandler');
 
 
@@ -49,7 +50,16 @@ describe('Frontend Configuration',()=>{
     expect(screen.getByDisplayValue('dbUrl')).toBeTruthy();
     expect(screen.getByDisplayValue('2')).toBeTruthy();
   });
-  it('should show updated API version  when API version textfield is updated',()=>{
+  it('should call FE config handler with port as parameter when port textfield is updated',()=>{
+    render(<FrontendConfig userRepo = {mockProp.userRepo} userRepoHandler = {mockProp.userRepoHandler} feConfig = {mockProp.feConfig} feConfigHandler = {mockProp.feConfigHandler} />);
+    expect(screen.getByDisplayValue('3000')).toBeTruthy();
+    expect(FeConfigHandler).toHaveBeenCalledTimes(0);
+    const PortTextfield = screen.getByDisplayValue('3000');
+    fireEvent.change( PortTextfield , {target : {value :'8000'}});
+    expect(FeConfigHandler).toHaveBeenCalledTimes(1);
+    expect(FeConfigHandler).toHaveBeenCalledWith(expect.anything(),'port',expect.anything(),expect.anything());
+  });
+  it('should call FE config handler with reactVersion as parameter when API version textfield is updated',()=>{
     render(<FrontendConfig userRepo = {mockProp.userRepo} userRepoHandler = {mockProp.userRepoHandler} feConfig = {mockProp.feConfig} feConfigHandler = {mockProp.feConfigHandler} />);
     expect(screen.getByDisplayValue('4.2')).toBeTruthy();
     expect(FeConfigHandler).toHaveBeenCalledTimes(0);
@@ -60,77 +70,67 @@ describe('Frontend Configuration',()=>{
 
   });
  
-  // it('should show updated Port  when port textfield is updated',()=>{
-  //   render(<FrontendConfig userRepo = {mockProp.userRepo} userRepoHandler = {mockProp.userRepoHandler} feConfig = {mockProp.feConfig} feConfigHandler = {mockProp.feConfigHandler} />);
-  //   expect(screen.getByDisplayValue('3000')).toBeTruthy();
-  //   expect(FeConfigHandler).toHaveBeenCalledTimes(0);
-  //   const PortTextfield = screen.getByLabelText('Enter Port number');
-  //   fireEvent.change( PortTextfield , {target : {value :'8000'}});
-  //   expect(FeConfigHandler).toHaveBeenCalledTimes(1);
-  //   expect(FeConfigHandler).toHaveBeenCalledWith(expect.anything(),'port',expect.anything(),expect.anything());
+  it('should call FE config handler with noOfReplica as parameter when Replica number textfield is updated',()=>{
+    render(<FrontendConfig userRepo = {mockProp.userRepo} userRepoHandler = {mockProp.userRepoHandler} feConfig = {mockProp.feConfig} feConfigHandler = {mockProp.feConfigHandler} />);
+    expect(screen.getByDisplayValue('2')).toBeTruthy();
+    expect(FeConfigHandler).toHaveBeenCalledTimes(0);
+    const ReplicaTextfield = screen.getByDisplayValue('2');
+    fireEvent.change( ReplicaTextfield , {target : {value :'1'}});
+    expect(FeConfigHandler).toHaveBeenCalledTimes(1);    
+    expect(FeConfigHandler).toHaveBeenCalledWith(expect.anything(),'noOfReplica',expect.anything(),expect.anything());
 
-
-  // });
-  // it('should show updated Replica number when Replica number textfield is updated',()=>{
-  //   render(<FrontendConfig userRepo = {mockProp.userRepo} userRepoHandler = {mockProp.userRepoHandler} feConfig = {mockProp.feConfig} feConfigHandler = {mockProp.feConfigHandler} />);
-  //   expect(screen.getByDisplayValue('2')).toBeTruthy();
-  //   expect(FeConfigHandler).toHaveBeenCalledTimes(0);
-  //   const ReplicaTextfield = screen.getByLabelText('Enter Replica required');
-  //   fireEvent.change( ReplicaTextfield , {target : {value :'1'}});
-  //   expect(FeConfigHandler).toHaveBeenCalledTimes(1);    
-  //   expect(FeConfigHandler).toHaveBeenCalledWith(expect.anything(),'noOfReplica',expect.anything(),expect.anything());
-
-  // });
+  });
   
-  // it('should show updated  React app name when React app textfield is updated',()=>{
-  //   render(<FrontendConfig userRepo = {mockProp.userRepo} userRepoHandler = {mockProp.userRepoHandler} feConfig = {mockProp.feConfig} feConfigHandler = {mockProp.feConfigHandler} />);
-  //   expect(screen.getByDisplayValue('my-app')).toBeTruthy();
-  //   expect(FeConfigHandler).toHaveBeenCalledTimes(0);
-  //   const AppNameTextfield = screen.getByLabelText('Enter React app name');
-  //   fireEvent.change( AppNameTextfield , {target : {value :'react-app'}});
-  //   expect(FeConfigHandler).toHaveBeenCalledTimes(1);
-  //   expect(FeConfigHandler).toHaveBeenCalledWith(expect.anything(),'name',expect.anything(),expect.anything());
+  it('should call FE config handler with my-app as parameter when React app textfield is updated',()=>{
+    render(<FrontendConfig userRepo = {mockProp.userRepo} userRepoHandler = {mockProp.userRepoHandler} feConfig = {mockProp.feConfig} feConfigHandler = {mockProp.feConfigHandler} />);
+    expect(screen.getByDisplayValue('my-app')).toBeTruthy();
+    expect(FeConfigHandler).toHaveBeenCalledTimes(0);
+    const AppNameTextfield = screen.getByDisplayValue('my-app');
+    fireEvent.change( AppNameTextfield , {target : {value :'react-app'}});
+    expect(FeConfigHandler).toHaveBeenCalledTimes(1);
+    expect(FeConfigHandler).toHaveBeenCalledWith(expect.anything(),'name',expect.anything(),expect.anything());
 
-  // });
-  // it('should show updated application environment when Environment textfield is updated',()=>{
-  //   render(<FrontendConfig userRepo = {mockProp.userRepo} userRepoHandler = {mockProp.userRepoHandler} feConfig = {mockProp.feConfig} feConfigHandler = {mockProp.feConfigHandler} />);
-  //   expect(screen.getByDisplayValue('env')).toBeTruthy();
-  //   expect(FeConfigHandler).toHaveBeenCalledTimes(0);
-  //   const EnvTextfield = screen.getByLabelText('Enter application environment');
-  //   fireEvent.change( EnvTextfield , {target : {value :'port=2000'}});
-  //   expect(FeConfigHandler).toHaveBeenCalledTimes(1);
-  //   expect(FeConfigHandler).toHaveBeenCalledWith(expect.anything(),'env',expect.anything(),expect.anything());
+  });
+  it('should call FE config handler with env as parameter when Environment textfield is updated',()=>{
+    render(<FrontendConfig userRepo = {mockProp.userRepo} userRepoHandler = {mockProp.userRepoHandler} feConfig = {mockProp.feConfig} feConfigHandler = {mockProp.feConfigHandler} />);
+    expect(screen.getByDisplayValue('env')).toBeTruthy();
+    expect(FeConfigHandler).toHaveBeenCalledTimes(0);
+    const EnvTextfield = screen.getByDisplayValue('env');
+    fireEvent.change( EnvTextfield , {target : {value :'port=2000'}});
+    expect(FeConfigHandler).toHaveBeenCalledTimes(1);
+    expect(FeConfigHandler).toHaveBeenCalledWith(expect.anything(),'env',expect.anything(),expect.anything());
 
-  // });
-  // it('should show updated database url when database url textfield is updated',()=>{
-  //   render(<FrontendConfig userRepo = {mockProp.userRepo} userRepoHandler = {mockProp.userRepoHandler} feConfig = {mockProp.feConfig} feConfigHandler = {mockProp.feConfigHandler} />);
-  //   expect(screen.getByDisplayValue('dbUrl')).toBeTruthy();
-  //   expect(FeConfigHandler).toHaveBeenCalledTimes(0);
-  //   const dbUrlTextfield = screen.getByLabelText('Enter database url');
-  //   fireEvent.change( dbUrlTextfield , {target : {value :'localhost'}});
-  //   expect(FeConfigHandler).toHaveBeenCalledTimes(1);
-  //   expect(FeConfigHandler).toHaveBeenCalledWith(expect.anything(),'dbUrl',expect.anything(),expect.anything());
+  });
+  it('should call FE config handler with dbUrl as parameter when database url textfield is updated',()=>{
+    render(<FrontendConfig userRepo = {mockProp.userRepo} userRepoHandler = {mockProp.userRepoHandler} feConfig = {mockProp.feConfig} feConfigHandler = {mockProp.feConfigHandler} />);
+    expect(screen.getByDisplayValue('dbUrl')).toBeTruthy();
+    expect(FeConfigHandler).toHaveBeenCalledTimes(0);
+    const dbUrlTextfield = screen.getByDisplayValue('dbUrl');
+    fireEvent.change( dbUrlTextfield , {target : {value :'localhost'}});
+    expect(FeConfigHandler).toHaveBeenCalledTimes(1);
+    expect(FeConfigHandler).toHaveBeenCalledWith(expect.anything(),'dbUrl',expect.anything(),expect.anything());
     
-  // });
-  // it('should show updated Server address when Server address textfield is updated',()=>{
-  //   jest.mock('../../../../utils/RepoConfigHandler');
-  //   render(<FrontendConfig userRepo = {mockProp.userRepo} userRepoHandler = {mockProp.userRepoHandler} feConfig = {mockProp.feConfig} feConfigHandler = {mockProp.feConfigHandler} />);
-  //   expect(screen.getByDisplayValue('http://github.com')).toBeTruthy();
-  //   expect(FeConfigHandler).toHaveBeenCalledTimes(0);
-  //   const ApiTextfield = screen.getByLabelText('Enter Server address');
-  //   fireEvent.change( ApiTextfield , {target : {value :'http://dockerhub.com'}});
-  //   expect(FeConfigHandler).toHaveBeenCalledTimes(1);
-  //   // expect(FeConfigHandler).toHaveBeenCalledWith(expect.anything(),'noOfReplica',expect.anything(),expect.anything());
+  });
+  it('shouldcall FE config handler with serverAddress as parameter when Server address textfield is updated',()=>{
+    render(<FrontendConfig userRepo = {mockProp.userRepo} userRepoHandler = {mockProp.userRepoHandler} feConfig = {mockProp.feConfig} feConfigHandler = {mockProp.feConfigHandler} />);
+    expect(screen.getByDisplayValue('http://github.com')).toBeTruthy();
+    expect(RepoConfigHandler).toHaveBeenCalledTimes(0);
+    const ServerTextfield = screen.getByDisplayValue('http://github.com');
+    fireEvent.change( ServerTextfield , {target : {value :'http://dockerhub.com'}});
+    expect(RepoConfigHandler).toHaveBeenCalledTimes(1);
+    expect(RepoConfigHandler).toHaveBeenCalledWith(expect.anything(),'serverAddress',expect.anything(),expect.anything());
     
-  // });
-  // it('should show updated Token value when Token textfield is updated',()=>{
-  //   render(<FrontendConfig userRepo = {mockProp.userRepo} userRepoHandler = {mockProp.userRepoHandler} feConfig = {mockProp.feConfig} feConfigHandler = {mockProp.feConfigHandler} />);
-  //   expect(screen.getByLabelText('Enter Server address')).toBeTruthy();
-  //   expect(RepoConfigHandler).toHaveBeenCalledTimes(0);
-  //   const ApiTextfield = screen.getByLabelText('Enter Server address');
-  //   fireEvent.change( ApiTextfield , {target : {value :'efg'}});
-  //   expect(RepoConfigHandler).toHaveBeenCalledTimes(1);
+  });
+  it('should call FE config handler with token as parameter when Token textfield is updated',()=>{
+    render(<FrontendConfig userRepo = {mockProp.userRepo} userRepoHandler = {mockProp.userRepoHandler} feConfig = {mockProp.feConfig} feConfigHandler = {mockProp.feConfigHandler} />);
+    expect(screen.getByDisplayValue('abcd')).toBeTruthy();
+    expect(RepoConfigHandler).toHaveBeenCalledTimes(0);
+    const TokenTextfield = screen.getByDisplayValue('abcd');
+    fireEvent.change( TokenTextfield , {target : {value :'efg'}});
+    expect(RepoConfigHandler).toHaveBeenCalledTimes(1);
+    expect(RepoConfigHandler).toHaveBeenCalledWith(expect.anything(),'token',expect.anything(),expect.anything());
+
     
-  // });
+  });
 
 });
