a81ea774c29708f1e7e839dd25ec7db235b66374
ReeKid2002 <rahulbehera2002@gmail.com>
2023-03-05 19:01:54 +0530

    feat: add component for microservices

--- src/components/MicroServices/index.js
+++ src/components/MicroServices/index.js
@@ -2,8 +2,28 @@ import React from 'react';
 import './MicroServices.css';
 
 const MicroServices = () => {
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData('application/reactflow', nodeType);
+    event.dataTransfer.effectAllowed = 'move';
+  };
   return (
-    <div></div>
+    <div className='microservices'>
+      <div>
+        <p className='heading'>MicroServices</p>
+        <hr />
+      </div>
+      <div className='services'>
+        <div draggable onDragStart={(event) => onDragStart(event, 'frontend')} className='service'>
+          <p>FRONTEND</p>
+        </div>
+        <div draggable onDragStart={(event) => onDragStart(event, 'backend')} className='service'>
+          <p>BACKEND</p>
+        </div>
+        <div draggable onDragStart={(event) => onDragStart(event, 'database')} className='service'>
+          <p>DATABASE</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
