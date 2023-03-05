d815bb33300e44ec3a091fb6a090a61bd92ad971
shashwath sk <shashukarkera01@gmail.com>
2023-03-05 20:33:34 +0530

    fix: update import paths

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ function App() {
       {/* <Route path='/' element={<Footer />}></Route>  */}
       <Route path='/dashboard' element={<ReactFlowProvider><MainDashboard /></ReactFlowProvider>}></Route>
       {/* <Route path='/' element={<Footer />}></Route>  */}
+      <Route path='/dashboard' element={<ReactFlowProvider><MainDashboard /></ReactFlowProvider>}></Route>
     </Routes>
   );
 }
