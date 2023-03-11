edeb5193a04dade91164ea72add9732b897f6076
Ashrene Roy <46489332+AshreneRoy@users.noreply.github.com>
2023-03-11 15:59:39 +0530

    Feature/drag and drop (#14)

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ function App() {
       <Route path='/dashboard' element={<ReactFlowProvider><MainDashboard /></ReactFlowProvider>}></Route>
       <Route path = '/configuration' element = {<ConfigurationPage/>}></Route>
       {/* <Route path='/' element={<Footer />}></Route>  */}
+      <Route path='/dashboard' element={<ReactFlowProvider><MainDashboard /></ReactFlowProvider>}></Route>
     </Routes>
   );
 }
