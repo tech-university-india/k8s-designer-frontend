1cde2778fb077f1c944ba07dafb9229efaba7123
shashwath sk <shashukarkera01@gmail.com>
2023-03-12 20:33:34 +0530

    chore: add testcases to check component is working as expected

--- src/components/CustomTextField/__test__/CustomTextField.test.jsx
+++ src/components/CustomTextField/__test__/CustomTextField.test.jsx
@@ -1,30 +1,25 @@
-import {fireEvent, render ,screen } from '@testing-library/react';
+import {fireEvent, render ,screen ,act} from '@testing-library/react';
 import CustomTextField from '..';
 import * as React from 'react';
 
 describe('Frontend Configuration',()=>{
 
   const mock = {
-    userConfig:{
-      PORT:'',
-      NUMBER_OF_REPLICAS : '',
-      ENVIRONMENT:'',
-      DATABASE_URL:''
-    },
-    handleUserConfig:jest.fn(),
-    setNewTextField:jest.fn()
+    Key: '0',
+    customConfig: ['',''],
+    handleCustomConfig:jest.fn(),
   };
 
   it('should show empty textfield  when component is rendered',()=>{
     expect(screen.queryByTestId('newConfig-key')).toBeFalsy();
-    render(<CustomTextField userConfig = {mock.userConfig} handleUserConfig={mock.handleUserConfig} setNewTextField = {mock.setNewTextField}/>);
+    render(<CustomTextField Key={mock.Key} customConfig={mock.customConfig} handleCustomConfig={mock.handleCustomConfig}/>);
     expect(screen.queryByTestId('newConfig-key')).toBeTruthy();
     expect(screen.queryByTestId('newConfig-key').value).toEqual(undefined);
   });
 
   it('should update newConfigKey when textfield is updated',()=>{
     expect(screen.queryByTestId('newConfig-key')).toBeFalsy();
-    render(<CustomTextField userConfig = {mock.userConfig} handleUserConfig={mock.handleUserConfig} setNewTextField = {mock.setNewTextField}/>);
+    render(<CustomTextField Key={mock.Key} customConfig={mock.customConfig} handleCustomConfig={mock.handleCustomConfig}/>);
     expect(screen.queryByTestId('newConfig-key')).toBeTruthy();
     expect(screen.queryByLabelText('Enter Key')).toBeTruthy();
     const configKey = screen.getByTestId('newConfig-key').querySelector('input');
@@ -32,22 +27,30 @@ describe('Frontend Configuration',()=>{
     expect(screen.queryByDisplayValue('name')).toBeTruthy();
   });
 
-  it('should show add config button when value-textfield has some value',()=>{
+  it('should update newConfigvalue when textfield is updated',()=>{
     expect(screen.queryByTestId('newConfig-value')).toBeFalsy();
-    render(<CustomTextField userConfig = {mock.userConfig} handleUserConfig={mock.handleUserConfig} setNewTextField = {mock.setNewTextField}/>);
-    expect(screen.queryByLabelText('Enter Value')).toBeTruthy();
-    const configKey = screen.getByTestId('newConfig-key').querySelector('input');
-    fireEvent.change(configKey,{target:{value:'name'}});
+    render(<CustomTextField Key={mock.Key} customConfig={mock.customConfig} handleCustomConfig={mock.handleCustomConfig}/>);
     expect(screen.queryByTestId('newConfig-value')).toBeTruthy();
-    const configValue = screen.queryByTestId('newConfig-value').querySelector('input');
-    fireEvent.change(configValue,{target:{value:'2020'}});
-    expect(screen.queryByDisplayValue('2020')).toBeTruthy();
-
+    const configKey = screen.getByTestId('newConfig-value').querySelector('input');
+    fireEvent.change(configKey,{target:{value:'name'}});
+    expect(screen.queryByDisplayValue('name')).toBeTruthy();
   });
 
-
+  it('should update customconfig object by calling event passed as prop when textfield is updated',()=>{
+    jest.useFakeTimers();
+    expect(screen.queryByTestId('newConfig-key')).toBeFalsy();
+    render(<CustomTextField Key={mock.Key} customConfig={mock.customConfig} handleCustomConfig={mock.handleCustomConfig}/>);
+    expect(screen.queryByTestId('newConfig-key')).toBeTruthy();
+    const configKey = screen.getByTestId('newConfig-key').querySelector('input');
+    expect(mock.handleCustomConfig).not.toHaveBeenCalled();
+    act(() => {
+      fireEvent.change(configKey,{target:{value:'name'}});
+    });
+    jest.advanceTimersByTime(1000);
+    expect(mock.handleCustomConfig).toHaveBeenCalledTimes(1);
+  });
   it('should render correctly and create a snapshot', () => {
-    const {asFragment } =  render(<CustomTextField userConfig = {mock.userConfig} handleUserConfig={mock.handleUserConfig} setNewTextField = {mock.setNewTextField}/>);
+    const {asFragment } =  render(<CustomTextField Key={mock.Key} customConfig={mock.customConfig} handleCustomConfig={mock.handleUserConfig}/>);
     expect(asFragment()).toMatchSnapshot();
   });
 });
